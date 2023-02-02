13e8cd0f295a77a7e2648ac7b3b48e7d248ef801
zhangyu <1802833394@qq.com>
2023-02-02 20:54:25 +0800

    整合Druid

--- springboot/springboot_07_druid/src/test/java/org/zzzzzz/Springboot07DruidApplicationTests.java
+++ springboot/springboot_07_druid/src/test/java/org/zzzzzz/Springboot07DruidApplicationTests.java
@@ -1,13 +1,20 @@
 package org.zzzzzz;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zzzzzz.dao.BookDao;
+import org.zzzzzz.domain.Book;
 
 @SpringBootTest
 class Springboot07DruidApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    void contextLoads() {
+        System.out.println(bookDao.getById(2));
+    }
 
 }
