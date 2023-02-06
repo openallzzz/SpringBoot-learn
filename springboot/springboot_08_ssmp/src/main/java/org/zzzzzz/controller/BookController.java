86cf1327f6b7950c403fb8d48b483a5a13f4c312
zhangyu <1802833394@qq.com>
2023-02-06 22:21:33 +0800

    表现层标准开发

---
+++ springboot/springboot_08_ssmp/src/main/java/org/zzzzzz/controller/BookController.java
@@ -0,0 +1,49 @@
+package org.zzzzzz.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import lombok.Lombok;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.zzzzzz.domain.Book;
+import org.zzzzzz.service.IBookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private IBookService bookService;
+
+    @GetMapping
+    public List<Book> getAll() {
+        return bookService.list();
+    }
+
+    @PostMapping
+    public Boolean save(@RequestBody Book book) {
+        return bookService.save(book);
+    }
+
+    @PutMapping
+    public Boolean update(@RequestBody Book book) {
+        return bookService.updateById(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean delete(@PathVariable Integer id) {
+        return bookService.removeById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Book getById(@PathVariable Integer id) {
+        return bookService.getById(id);
+    }
+
+    @GetMapping("/{currentPage}/{pageSize}")
+    public IPage<Book> getPage(@PathVariable int currentPage, @PathVariable int pageSize) {
+        return bookService.getPage(currentPage, pageSize);
+    }
+
+}
--- springboot/springboot_08_ssmp/src/main/java/org/zzzzzz/service/impl/BookServiceImpl2.java
+++ springboot/springboot_08_ssmp/src/main/java/org/zzzzzz/service/impl/BookServiceImpl2.java
@@ -1,6 +1,9 @@
 package org.zzzzzz.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.zzzzzz.dao.BookDao;
 import org.zzzzzz.domain.Book;
@@ -8,4 +11,14 @@ import org.zzzzzz.service.IBookService;
 
 @Service
 public class BookServiceImpl2 extends ServiceImpl<BookDao, Book> implements IBookService {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    public IPage<Book> getPage(int currentPage, int pageSize) {
+        IPage<Book> page = new Page<>(currentPage, pageSize);
+        bookDao.selectPage(page, null);
+        return page;
+    }
 }
